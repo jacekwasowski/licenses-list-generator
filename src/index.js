7d8f97f96647e2cf79e6035d0a18f7146fbdbef7
sam <samhavens@gmail.com>
2018-05-24 13:55:45 -0700

    Add print and save, BREAKING CHANGE.
    This changes what is exported. This allows for exporting a print and save functionality, currently only CSV is supported.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,15 @@ const fs = require('fs');
 const projectPath = process.cwd();
 const licenses = [];
 const paths = [];
+const LICENSE_FILES = [
+  'LICENSE',
+  'LICENSE.md',
+  'LICENSE.txt',
+  'LICENCE',
+  'LICENSE.markdown',
+  'LICENSE-MIT',
+  'LICENSE.rst',
+];
 
 function log(msg, path) {
   console.log(`licenses-list-generator: ${msg} ${path}`); // eslint-disable-line no-console
@@ -35,7 +44,7 @@ function getPackageJson(path = projectPath) {
 }
 
 function getLicenseText(path) {
-  const possibleNames = ['LICENSE', 'LICENSE.md', 'LICENSE.txt', 'LICENCE', 'LICENSE.markdown'];
+  const possibleNames = LICENSE_FILES;
   const name = possibleNames.find(fileName => isFile(`${path}/${fileName}`));
 
   if (!name) {
@@ -68,4 +77,44 @@ function getLicenses(path = projectPath) {
   return licenses;
 }
 
-module.exports = () => getLicenses(projectPath);
+function printLicenses(licensesArray) {
+  const stringifyType = (x) => {
+    if (x.type === null) {
+      x.type = 'NO LICENSE';// eslint-disable-line no-param-reassign
+      return x;
+    }
+
+    let y = x.type;
+    while (typeof y !== 'string') {
+      y = y.type;
+    }
+    x.type = y;// eslint-disable-line no-param-reassign
+    return x;
+  };
+
+  const rowify = (x) => {
+    const path = x.path.split('/');
+    const name = x.name || path[path.length - 1];
+    return `${name}, ${x.type}`;
+  };
+
+  return licensesArray.map(stringifyType).map(rowify).join('\n');
+}
+
+function saveLicenses(licensesInpt, path = './licenses.csv') {
+  let licenseString = licensesInpt;
+  if (typeof licenseString !== 'string') {
+    licenseString = printLicenses(licenses);
+  }
+  fs.writeFile(path, licenseString, 'utf8', (err) => {
+    if (err) throw err;
+    console.log(`Saved ${path}`); // eslint-disable-line no-console
+  });
+}
+
+module.exports = {
+  get: () => getLicenses(projectPath),
+  getLicenses,
+  printCsv: printLicenses,
+  saveCsv: saveLicenses,
+};
